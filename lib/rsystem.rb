386d5be2f4caafe651584afd7ae489236cbfc792
Miles Jordan <miles@milesjordan.com.au>
2015-02-08 14:23:06 +1100

    Whitespace fixes

--- lib/rsystem.rb
+++ lib/rsystem.rb
@@ -2,26 +2,26 @@ require "rsystem/version"
 
 module RSystem
   class Runner
-  	def self.run(command, options={})
+    def self.run(command, options={})
 
-  	  # Only set the default options of the haven't already been set
-  	  options[:echo] = true unless options.has_key?(:echo)
-  	  options[:raise_on_error] = true unless options.has_key?(:raise_on_error)
+      # Only set the default options of the haven't already been set
+      options[:echo] = true unless options.has_key?(:echo)
+      options[:raise_on_error] = true unless options.has_key?(:raise_on_error)
 
-  	  puts "[#{Time.now}] Executing: #{command}" if options[:echo]
+      puts "[#{Time.now}] Executing: #{command}" if options[:echo]
 
-  	  # Remove custom options so we can pass the entire options hash to system()
+      # Remove custom options so we can pass the entire options hash to system()
       sys_options = options.clone
-	    sys_options.delete(:echo)
-	    sys_options.delete(:raise_on_error)
+      sys_options.delete(:echo)
+      sys_options.delete(:raise_on_error)
 
-  	  ret = system(command, sys_options)
-  	  if !ret and options[:raise_on_error]
-  	  	raise FailedExecutionException, "[#{Time.now}] Error: The executed command returned a failure code."
-  	  else
-  	  	ret
-  	  end
-  	end
+      ret = system(command, sys_options)
+      if !ret and options[:raise_on_error]
+        raise FailedExecutionException, "[#{Time.now}] Error: The executed command returned a failure code."
+      else
+        ret
+      end
+    end
   end
 
   class FailedExecutionException < Exception
